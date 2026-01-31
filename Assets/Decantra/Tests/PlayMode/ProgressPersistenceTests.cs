38069620d0ba17c4952921a485ea66b44826f679
Christian Gleissner <chrisgleissner@gmail.com>
2026-01-31 23:12:56 +0000

    Add license header to all source files
    This commit adds a license header to all source files in the Decantra project

--- Assets/Decantra/Tests/PlayMode/ProgressPersistenceTests.cs
+++ Assets/Decantra/Tests/PlayMode/ProgressPersistenceTests.cs
@@ -1,3 +1,11 @@
+/*
+Decantra - A Unity-based bottle-sorting puzzle game
+Copyright (C) 2026 Christian Gleissner
+
+Licensed under the GNU General Public License v2.0 or later.
+See <https://www.gnu.org/licenses/> for details.
+*/
+
 using System;
 using System.IO;
 using Decantra.App.Services;
